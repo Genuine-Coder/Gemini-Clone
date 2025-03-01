c02f10c6cf3981bfb8e84aa3b9d45a06fdb20561
Pravesh Kumar <praveshkumar5254@gmail.com>
2025-03-01 21:15:14 +0530

    Chat Section Added (frontend end )

--- frontend/src/Components/ChatSection/ChatSection.jsx
+++ frontend/src/Components/ChatSection/ChatSection.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import './ChatSection.css'
-import Mode from '../Mode/Mode'
+import React from "react";
+import "./ChatSection.css";
+import Mode from "../Mode/Mode";
+import { LuSendHorizontal } from "react-icons/lu";
 
 export default function ChatSection() {
   return (
     <div className="chatSection">
-        <div className="topSection">
-            
+      <div className="topSection">
+        <div className="heading">
+          <span>Hello Surya,</span>
+          <span>I'm Your Own Assistant</span>
+          <span>How can I help you...?</span>
         </div>
-        <div className="bottomSection">
-
+      </div>
+      <div className="bottomSection">
+        <div className="bottom">
+        <input type="text" placeholder="enter your prompt.." id="inp"></input>
+        <button id="sendBtn"> <LuSendHorizontal/></button>
+        <Mode/>
         </div>
         
-    </div>
 
-  )
+      </div>
+    </div>
+  );
 }
